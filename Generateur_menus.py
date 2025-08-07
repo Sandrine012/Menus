cd3ba732c4b40ddea92123ea67da09c92a6276a5
Sandrine012 <overlous047@gmail.com>
2025-08-07 11:23:58 +0200

    Update Generateur_menus.py

--- Generateur_menus.py
+++ Generateur_menus.py
@@ -1251,7 +1251,7 @@ def main():
                 st.sidebar.download_button(
                     label="Télécharger le fichier CSV",
                     data=output,
-                    file_name="fichier_gdrive.csv",
+                    file_name="Planning.csv",
                     mime="text/csv"
                 )
                 
